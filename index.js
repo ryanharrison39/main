29da7167471902bef5fbe2478c612be53863c70a
Ryan Harrison <s4165116@student.rmit.edu.au>
2025-05-27 19:56:55 +1000

    fixed navbar visual hover shit

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+let isMouseOver = true;
 
 const navbar = document.getElementById("nav-bar");
 
@@ -8,37 +9,48 @@ const dropdowns = document.getElementsByClassName("dropdown");
 
 for (let dropdown of dropdowns) {
   dropdown.addEventListener("mouseover", function() {
-    
+    isMouseOver = true;
     navbar.style.height = "200px";
     dropdownNavMouseArea.style.height = "170px";
     setTimeout(function() {dropdownNavText.style.opacity = "1";}, 200);
 
 
-    dropdownNavText.addEventListener("mouseover", function() {
-    navbar.style.height = "200px";
-    dropdownNavText.style.opacity = "1";
-
-});
-});
+    
+  });
 }
 
+dropdownNavText.addEventListener("mouseover", function() {
+    isMouseOver = true;
+    });
 
+dropdownNavMouseArea.addEventListener("mouseover", function() {
+    isMouseOver = true;
+    });
+
+dropdownNavText.addEventListener("mouseout", function() {
+    isMouseOver = false;
+    });
 
 dropdownNavMouseArea.addEventListener("mouseout", function() {
+    
+  setTimeout(function() {
+    if (!isMouseOver) {
+      dropdownNavText.removeEventListener("mouseover", function() {
+        navbar.style.height = "200px";
+        dropdownNavText.style.opacity = "1";
 
-    setTimeout(function() {
-          dropdownNavText.removeEventListener("mouseover", function() {
-          navbar.style.height = "200px";
-          dropdownNavText.style.opacity = "1";
-
-        });
+      });
 
 
-          dropdownNavText.style.display = "hidden";
-          dropdownNavText.style.opacity = "0";
-          navbar.style.height = "50px";
-          dropdownNavMouseArea.style.height = "0";
-        },200);
+      dropdownNavText.style.display = "hidden";
+      dropdownNavText.style.opacity = "0";
+      navbar.style.height = "50px";
+      dropdownNavMouseArea.style.height = "0";
+    }
+  },200);
+
+  isMouseOver = false;                
+                  
 });
 
 
